b14663a4824f397000ffe6bf712fa3733eb5b16c
Alireza Rashidi <arashidi1378@gmail.com>
2021-04-19 20:43:34 +0430

    uploading project files.

---
+++ scripts/index2.js
@@ -0,0 +1,209 @@
+
+/*
+***NOTE that some logs are printed on the inspect-elements in browser
+ you can see this logs for step by step oprations.
+ for example for functions like setCookie and deleteCookie
+ i inserted logs for better understanding of oprations.
+*/
+
+function setInLocalStorage(nameVal, gender) {
+
+   // saving to local storage in form (key=name, value=gender)
+   localStorage.setItem(nameVal, gender);  
+   console.log("name=" + nameVal + " gender= " + gender + " stored in local storage." );
+  }
+ 
+  /*
+  deleteFromLocalStorage():
+  this function deletes record using name of user from local storage.
+  ***NOTE that some logs are printed on the inspect-elements in browser
+  you can see this logs for step by step oprations.
+  */
+ 
+function deleteFromLocalStorage(nameVal) {
+
+    // removing name of user from local storage
+   var storedGender = getFromLocalStorage(nameVal)
+   localStorage.removeItem(nameVal);
+   console.log("name=" + nameVal + " gender= " + storedGender + " removed from storage." );
+  }
+
+  /*
+    getFromLocalStorage():
+    this function passes the key (name of user) and retrives the 
+    he/she's gender.
+  */
+ 
+function getFromLocalStorage(nameVal) {  // search in local storage by name.
+   var storedGender = localStorage.getItem(nameVal);
+   return storedGender;
+  }
+ 
+ /*
+ createErrorMsg:
+    if some kind of error happen, show this error to the user.
+ */
+function createErrorMsg(msg){
+ 
+   var errorMsg = document.getElementById("errorMessage");
+   errorMsg.innerHTML = msg;
+ }
+ 
+ /*
+ createErrorMsg:
+    this function clears error message every time the submit event happens.
+ */
+ 
+function removeErrorMsg(){
+    
+   var errorMsg = document.getElementById("errorMessage");
+   errorMsg.innerHTML = "";
+ }
+
+ /*
+ resetSavedMsgTextBox:
+    this function clears SavedMsgTextBox.
+ */
+ 
+function resetSavedMsgTextBox(){
+
+   var savedMessageTextBox = document.getElementById("savedAnswerTextBox");
+   savedMessageTextBox.value = "";
+ }
+
+  /*
+ resetPredictionTextBox:
+    this function clears PredictionTextBox.
+ */
+
+function resetPredictionTextBox(){
+
+   var PredictionTextBox = document.getElementById("predictionTextBox")
+   PredictionTextBox.value = "";
+}
+
+/*
+   resetPredictionTextBox:
+   this function clears radio Group.
+*/
+
+function resetRadioGroup(){
+
+   document.getElementById("femaleBtn").checked = false;
+   document.getElementById("maleBtn").checked = false;
+}
+
+ let genderValue = "";
+ let nameValue = "";
+ let validInput;
+ 
+ // using DOM to access to submit button to create an event.
+ let submitBtn = document.getElementById("submitBtn");
+ 
+ submitBtn.addEventListener("click", ()=>{
+ 
+     console.log('submit btn clicked!!');
+     removeErrorMsg();           // removing the error msg.
+     resetSavedMsgTextBox();     // reset textbox
+     resetPredictionTextBox();   // reset textbox
+     genderValue = "";
+     nameValue = "";
+     validInput = true;          // reseting the flag for next user.
+    /*
+       after submit has been clicked, we read the string and radio button values
+       that user had been entered using DOM.
+    */
+      nameValue = document.getElementById("nameTxtBox").value;   // gating the name from user.
+      let GenderRadioGroup = document.getElementsByName("gender");
+
+      if(nameValue === "" || GenderRadioGroup === "")
+         createErrorMsg("Enter name and gender!");
+
+      else{
+         //------------ checking the user selection according to radio buttons ---------
+         if(GenderRadioGroup[0].checked)
+            genderValue = "male";
+         else if (GenderRadioGroup[1].checked)
+            genderValue = "female";
+   
+      //   console.log(nameValue);
+      //   console.log(genderValue);
+   
+         const SERVER_URL = new URL("https://api.genderize.io");
+   
+         /*
+            creating params using user's information: 
+         */
+   
+         params = {name:nameValue, gender:genderValue};     // convertion to param object.
+         console.log(params);   // printing log in inspect-element in browser.
+   
+         //------------- apending quary params to url
+         Object.keys(params).forEach(key => SERVER_URL.searchParams.append(key, params[key]))
+         let httpResponse;
+
+         /* 
+            using fetch function to send GET request to server
+            this function returns a promise object, and I used
+            chaining them to convert the response object to json
+            format. and in inner then, I used json data recived 
+            from server to show to user.
+      */
+   
+         httpResponse = fetch(SERVER_URL).then(response => response.json())
+         .then(json => {console.log(json); 
+         
+         let predictionTextBox = document.getElementById("predictionTextBox");
+         
+         /*
+            this part is for handling the server problem or some name
+            prediction problem we will throw some alert message to user.
+         */
+   
+         if(json["gender"] === null && json["probability"] === 0){
+            createErrorMsg("Error! please enter another name");
+            validInput = false;
+         }
+   
+         /*
+         else: if we dont have any problems, we pass server 's response information
+         that has json format(name=value) to the predictionTextBox object
+         using DOM.
+         */
+         else{ 
+            predictionTextBox.value = json["gender"]  + "\n";
+            predictionTextBox.value += json["probability"];
+         }
+
+         if(validInput && genderValue !== "")           // if has valid input and gender value store it.
+            setInLocalStorage(nameValue, genderValue); // saving true values from user. pair(name, gender)
+   
+         // search for cookie name and if it was cookie for that name, show in TextBox.
+         
+         storageValue = getFromLocalStorage(nameValue);
+
+         if(storageValue !== null){   // if user had saved information.
+            let savedMessageTextBox = document.getElementById("savedAnswerTextBox");
+            savedMessageTextBox.value = storageValue;
+         }
+         
+      })
+         .catch(err =>{
+            createErrorMsg("Error! Network problem.");
+            validInput = false});   // error handeling. (network problem!!)
+      }
+      resetRadioGroup();           // reset radioGroup
+ });
+ /*
+  setting event handler for clear button if the user press the button the 
+  callback function(arrow function) body will run.
+ */
+let clearBtn = document.getElementById("clearBtn"); 
+clearBtn.addEventListener("click", ()=>{
+    
+   resetSavedMsgTextBox();
+   deleteFromLocalStorage(nameValue);
+});
+ 
+ 
+ 
